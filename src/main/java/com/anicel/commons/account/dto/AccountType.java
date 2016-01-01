2674f88371dea3c538f203c3c3f6496cf1e29a7b
zhangzhaoyu <zhangzhaoyu0524@163.com>
2016-01-01 22:58:29 +0800

    update

--- src/main/java/com/anicel/commons/account/dto/AccountType.java
+++ src/main/java/com/anicel/commons/account/dto/AccountType.java
@@ -4,5 +4,5 @@ package com.anicel.commons.account.dto;
  * Created by zhaoyu on 15-10-20.
  */
 public enum AccountType {
-    ROOT, PERSONAL, AccountType, AccountType, ORGANIZATIONAL
+    ROOT, PERSONAL, ORGANIZATIONAL
 }
