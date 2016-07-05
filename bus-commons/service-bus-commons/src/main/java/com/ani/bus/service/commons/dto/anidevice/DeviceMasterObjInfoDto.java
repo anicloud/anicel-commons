4ef5516d5500b654d49040b2516a46c54f52d16d
zhangzhaoyu <zhangzhaoyu0524@163.com>
2016-07-05 18:32:01 +0800

    update master slave ref

--- bus-commons/service-bus-commons/src/main/java/com/ani/bus/service/commons/dto/anidevice/DeviceMasterObjInfoDto.java
+++ bus-commons/service-bus-commons/src/main/java/com/ani/bus/service/commons/dto/anidevice/DeviceMasterObjInfoDto.java
@@ -10,6 +10,7 @@ import com.ani.octopus.commons.object.enumeration.DataState;
 import com.ani.octopus.commons.stub.dto.StubDto;
 import com.ani.utils.core.AniByte;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
@@ -18,27 +19,43 @@ import java.util.Map;
  * @date 16-3-8
  * @since JDK 1.7
  */
-public class DeviceMasterObjInfoDto extends ObjectMainInfoDto {
+public class DeviceMasterObjInfoDto implements Serializable {
     private static final long serialVersionUID = -2439991687697415895L;
 
     public String name;
     public String description;
 
+    public List<StubDto> stubs;
+    public DataState dataState;
+
+    public Long objectId;
+    public String uniId;
+    public AccountDto owner;
+    public AniObjectType objectType;
+    public List<ObjectMainPrivilegeDto> privileges;
+    public Map<AniByte, AniObjectState> hostsState;
+    public List<DeviceSlaveObjInfoDto> slaves;
+
     public DeviceMasterObjInfoDto() {
     }
 
-    public DeviceMasterObjInfoDto(String name, String description) {
+    public DeviceMasterObjInfoDto(String name, String description, List<StubDto> stubs,
+                                  DataState dataState, Long objectId, String uniId,
+                                  AccountDto owner, AniObjectType objectType,
+                                  List<ObjectMainPrivilegeDto> privileges,
+                                  Map<AniByte, AniObjectState> hostsState,
+                                  List<DeviceSlaveObjInfoDto> slaves) {
         this.name = name;
         this.description = description;
-    }
-
-    public DeviceMasterObjInfoDto(String name, String description, List<StubDto> stubs, DataState dataState, Long objectId,
-                                  String uniId, AccountDto owner, AniObjectType objectType,
-                                  List<ObjectMainPrivilegeDto> privileges, Map<AniByte, AniObjectState> hostsState,
-                                  List<ObjectSlaveInfoDto> slaves) {
-        super(stubs, dataState, objectId, uniId, owner, objectType, privileges, hostsState, slaves);
-        this.name = name;
-        this.description = description;
+        this.stubs = stubs;
+        this.dataState = dataState;
+        this.objectId = objectId;
+        this.uniId = uniId;
+        this.owner = owner;
+        this.objectType = objectType;
+        this.privileges = privileges;
+        this.hostsState = hostsState;
+        this.slaves = slaves;
     }
 }
 
