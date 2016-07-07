6a5ded830e76b5d1783e88ccf3dc6f3c463c37ab
zhangzhaoyu <zhangzhaoyu0524@163.com>
2016-07-07 17:08:59 +0800

    add ani-utils jar

--- bus-commons/service-bus-commons/src/main/java/com/ani/bus/service/commons/dto/anidevice/DeviceMasterObjInfoDto.java
+++ bus-commons/service-bus-commons/src/main/java/com/ani/bus/service/commons/dto/anidevice/DeviceMasterObjInfoDto.java
@@ -1,8 +1,6 @@
 package com.ani.bus.service.commons.dto.anidevice;
 
 import com.ani.octopus.commons.accout.dto.AccountDto;
-import com.ani.octopus.commons.object.dto.object.ObjectMainInfoDto;
-import com.ani.octopus.commons.object.dto.object.ObjectSlaveInfoDto;
 import com.ani.octopus.commons.object.dto.object.privilege.ObjectMainPrivilegeDto;
 import com.ani.octopus.commons.object.enumeration.AniObjectState;
 import com.ani.octopus.commons.object.enumeration.AniObjectType;
