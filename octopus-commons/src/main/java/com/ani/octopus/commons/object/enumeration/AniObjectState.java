67556e9b026b3a41433b280a3c32b45c07d584af
zsl <zsl_hit@qq.com>
2016-11-15 18:21:43 +0800

    fix the update slaves bugs

--- octopus-commons/src/main/java/com/ani/octopus/commons/object/enumeration/AniObjectState.java
+++ octopus-commons/src/main/java/com/ani/octopus/commons/object/enumeration/AniObjectState.java
@@ -3,7 +3,7 @@ package com.ani.octopus.commons.object.enumeration;
 /**
  * Created by yeh on 15-10-16.
  */
-public enum AniObjectState {
+public enum  AniObjectState {
     ACTIVE,
     DISABLE,
     REMOVED;
