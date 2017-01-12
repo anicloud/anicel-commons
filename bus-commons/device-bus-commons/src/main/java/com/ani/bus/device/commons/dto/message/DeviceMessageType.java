7db4579a6060cb44de8d7d6a033bf92c737f3aef
Ben Xu <xuben.cn@gmail.com>
2017-01-12 19:51:17 +0800

    modify the value of message type

--- bus-commons/device-bus-commons/src/main/java/com/ani/bus/device/commons/dto/message/DeviceMessageType.java
+++ bus-commons/device-bus-commons/src/main/java/com/ani/bus/device/commons/dto/message/DeviceMessageType.java
@@ -1,22 +1,22 @@
 package com.ani.bus.device.commons.dto.message;
 
 /**
- * Created by huangbin on 10/27/15.
- * Modified by xuben on 11/08/16.
+ * Created by huangbin on 15/10/27
+ * Modified by xuben on 17/01/12
  */
 public enum DeviceMessageType {
-    REGISTER_REQUEST(0),
-    REGISTER_RESPONSE(1),
-    AUTH_REQUEST(2),
-    AUTH_RESPONSE(3),
-    UPDATE_REQUEST(4),
-    UPDATE_RESPONSE(5),
-    INVOKE_REQUEST(6),
-    INVOKE_RESPONSE(7),
-    TIME_SYNC_REQUEST(8),
-    TIME_SYNC_RESPONSE(9),
-    INVOKE_SERVICE(10),
-    UNKNOWN(11);
+    REGISTER_REQUEST(1),
+    REGISTER_RESPONSE(2),
+    AUTH_REQUEST(3),
+    AUTH_RESPONSE(4),
+    UPDATE_REQUEST(5),
+    UPDATE_RESPONSE(6),
+    INVOKE_REQUEST(7),
+    INVOKE_RESPONSE(8),
+    TIME_SYNC_REQUEST(9),
+    TIME_SYNC_RESPONSE(10),
+    INVOKE_SERVICE(11),
+    UNKNOWN(12);
 
     private Integer value;
 
@@ -30,27 +30,27 @@ public enum DeviceMessageType {
 
     public static DeviceMessageType getType(Integer value) {
         switch (value) {
-            case 0:
-                return REGISTER_REQUEST;
             case 1:
-                return REGISTER_RESPONSE;
+                return REGISTER_REQUEST;
             case 2:
-                return AUTH_REQUEST;
+                return REGISTER_RESPONSE;
             case 3:
-                return AUTH_RESPONSE;
+                return AUTH_REQUEST;
             case 4:
-                return UPDATE_REQUEST;
+                return AUTH_RESPONSE;
             case 5:
-                return UPDATE_RESPONSE;
+                return UPDATE_REQUEST;
             case 6:
-                return INVOKE_REQUEST;
+                return UPDATE_RESPONSE;
             case 7:
-                return INVOKE_RESPONSE;
+                return INVOKE_REQUEST;
             case 8:
-                return TIME_SYNC_REQUEST;
+                return INVOKE_RESPONSE;
             case 9:
-                return TIME_SYNC_RESPONSE;
+                return TIME_SYNC_REQUEST;
             case 10:
+                return TIME_SYNC_RESPONSE;
+            case 11:
                 return INVOKE_SERVICE;
             default:
                 return UNKNOWN;
