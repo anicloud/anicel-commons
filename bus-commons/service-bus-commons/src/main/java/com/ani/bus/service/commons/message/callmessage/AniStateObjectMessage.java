b27a2434e038f8a48e6e790a156f2dca14a3d5cb
LiHui <812486664@qq.com>
2017-05-03 11:11:52 +0800

    Merge branch 'master' of https://github.com/anicloud/anibus-pro
    # Conflicts:
    #	device-bus/device-bus-core/src/main/java/com/ani/bus/device/application/antenna/StateObjectHandler.java

--- bus-commons/service-bus-commons/src/main/java/com/ani/bus/service/commons/message/callmessage/AniStateObjectMessage.java
+++ bus-commons/service-bus-commons/src/main/java/com/ani/bus/service/commons/message/callmessage/AniStateObjectMessage.java
@@ -1,8 +1,10 @@
 package com.ani.bus.service.commons.message.callmessage;
 
 import com.ani.bus.service.commons.dto.anidevice.DeviceStateDto;
+import com.ani.bus.service.commons.message.MessageType;
 import com.ani.bus.service.commons.message.SocketMessage;
 import com.ani.octopus.commons.state.dto.StateMachineTransDto;
+import com.ani.octopus.commons.state.dto.StateTransDto;
 
 import java.util.List;
 
@@ -11,10 +13,14 @@ import java.util.List;
  */
 public class AniStateObjectMessage extends SocketMessage {
 
-    public StateMachineTransDto stateMachineTransDto;
+    public Long deviceId;
+    public Integer slaveId;
+    public List<StateTransDto> stateTransDtos;
 
-    public AniStateObjectMessage(StateMachineTransDto stateMachineTransDto) {
-        this.stateMachineTransDto = stateMachineTransDto;
+    public AniStateObjectMessage(ResultCode resultCode, String msg, MessageType messageType, Long deviceId, Integer slaveId, List<StateTransDto> stateTransDtos) {
+        super(resultCode, msg, messageType);
+        this.deviceId = deviceId;
+        this.slaveId = slaveId;
+        this.stateTransDtos = stateTransDtos;
     }
-
 }
