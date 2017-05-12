8d79c0269e7742e2d46f4a0082be79da222c6ad1
zsl <zsl_hit@qq.com>
2017-05-12 14:21:53 +0800

    add heartbeat

---
+++ bus-commons/device-bus-commons/src/main/java/com/ani/bus/device/commons/dto/message/HeartBeatRequestContent.java
@@ -0,0 +1,21 @@
+package com.ani.bus.device.commons.dto.message;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created by zsl on 17-5-12.
+ */
+public class HeartBeatRequestContent extends DeviceMessageContent {
+    public HeartBeatRequestContent() {
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+    }
+
+    @Override
+    public void read(DataInput in) throws IOException {
+    }
+}
---
+++ bus-commons/device-bus-commons/src/main/java/com/ani/bus/device/commons/dto/message/HeartBeatResponseContent.java
@@ -0,0 +1,23 @@
+package com.ani.bus.device.commons.dto.message;
+
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Created by zsl on 17-5-12.
+ */
+public class HeartBeatResponseContent extends DeviceMessageContent {
+    public HeartBeatResponseContent() {
+    }
+
+    @Override
+    public void write(DataOutput out) throws IOException {
+
+    }
+
+    @Override
+    public void read(DataInput in) throws IOException {
+
+    }
+}
