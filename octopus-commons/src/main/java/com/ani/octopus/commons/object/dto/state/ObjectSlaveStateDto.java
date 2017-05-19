6e015895516fab541a5b2f0e4c9ff62391ae1cb4
zsl <zsl_hit@qq.com>
2017-05-19 15:46:01 +0800

    fix slavestates bug

--- octopus-commons/src/main/java/com/ani/octopus/commons/object/dto/state/ObjectSlaveStateDto.java
+++ octopus-commons/src/main/java/com/ani/octopus/commons/object/dto/state/ObjectSlaveStateDto.java
@@ -1,6 +1,7 @@
 package com.ani.octopus.commons.object.dto.state;
 
 import com.ani.octopus.commons.object.enumeration.AniObjectState;
+import com.ani.octopus.commons.state.dto.StateMachineDto;
 import com.ani.utils.dto.AniRequiredField;
 
 import java.util.Map;
@@ -13,6 +14,8 @@ public class ObjectSlaveStateDto extends ObjectStateDto {
     @AniRequiredField
 //    public Map<Integer, AniObjectState> slavesState;
     public AniObjectState slaveState;
+    public Integer slaveId;
+
     public ObjectSlaveStateDto() {
     }
 
@@ -24,5 +27,11 @@ public class ObjectSlaveStateDto extends ObjectStateDto {
     public ObjectSlaveStateDto(Long objectMainId, AniObjectState slaveState) {
         super(objectMainId);
         this.slaveState = slaveState;
+    }
+
+    public ObjectSlaveStateDto(Long objectMainId, Map<Integer, StateMachineDto> stateMachines, AniObjectState slaveState, Integer slaveId) {
+        super(objectMainId, stateMachines);
+        this.slaveState = slaveState;
+        this.slaveId = slaveId;
     }
 }
