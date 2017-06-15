d5469f533f9ab337728876ac03e9261f82f29b5e
zsl <zsl_hit@qq.com>
2017-06-15 15:59:19 +0800

    fix

--- bus-commons/device-bus-commons/src/main/java/com/ani/bus/device/commons/dto/message/UpdateSingleRequestContent.java
+++ bus-commons/device-bus-commons/src/main/java/com/ani/bus/device/commons/dto/message/UpdateSingleRequestContent.java
@@ -1,29 +1,92 @@
 package com.ani.bus.device.commons.dto.message;
 
 import com.ani.bus.device.commons.dto.device.DeviceSlaveDto;
+import com.ani.bus.device.commons.dto.device.FunctionDto;
+import com.ani.bus.device.commons.dto.util.MessageUtils;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by zsl on 17-6-13.
  */
 public class UpdateSingleRequestContent extends DeviceMessageContent {
-    public DeviceSlaveDto deviceSlaveDto;
-    public boolean isRemoved;
+
+    public Integer deviceId;
+    public Boolean isRemoved;
+    public Integer physicalId;
+    public Long physicalAddress;
+    public String name;
+    public String description;
+
+    public List<FunctionDto> functions;
+
+    public String avatarUrl;
+    public List<Integer> tags;
+
+
+    public Boolean isactive;
 
     @Override
     public void write(DataOutput out) throws IOException {
+        out.writeInt(deviceId);
         out.writeBoolean(isRemoved);
-        deviceSlaveDto.write(out);
+        if (isRemoved) return;
+        out.writeInt(physicalId);
+        out.writeLong(physicalAddress);
+        MessageUtils.writeString(out, name);
+        MessageUtils.writeString(out, description);
+        MessageUtils.writeString(out, avatarUrl);
+        if (tags == null) {
+            out.writeShort(0);
+        } else {
+            out.writeShort(tags.size());
+            for (Integer tag : tags) {
+                out.writeInt(tag);
+            }
+        }
+        out.writeBoolean(isactive);
+        if (functions == null) {
+            out.writeShort(0);
+        } else {
+            out.writeShort(functions.size());
+            for (FunctionDto functionDto : functions) {
+                functionDto.write(out);
+            }
+        }
     }
 
     @Override
     public void read(DataInput in) throws IOException {
-        if (!(isRemoved = in.readBoolean())) {
-            deviceSlaveDto = new DeviceSlaveDto();
-            deviceSlaveDto.read(in);
+        deviceId = in.readInt();
+        isRemoved = in.readBoolean();
+        if (isRemoved) return;
+        physicalId = in.readInt();
+        physicalAddress = in.readLong();
+        name = MessageUtils.readString(in);
+        description = MessageUtils.readString(in);
+        avatarUrl = MessageUtils.readString(in);
+        int size = in.readShort();
+        if (size > 0) {
+            tags = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                tags.add(in.readInt());
+            }
+        }
+//        masterId = in.readLong();
+        isactive = in.readBoolean();
+
+        size = in.readShort();
+        if (size > 0) {
+            functions = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                FunctionDto functionDto = new FunctionDto();
+                functionDto.read(in);
+                functions.add(functionDto);
+            }
         }
     }
 }
